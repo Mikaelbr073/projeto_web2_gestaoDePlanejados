2ac22cd1fb0096b77f8096a62edfa58ba06b5403
Mikael Carvalho <70547713+Mikaelbr073@users.noreply.github.com>
2022-05-09 02:22:59 -0300

    Update Main.java

--- solid_lsp/src/Main/Main.java
+++ solid_lsp/src/Main/Main.java
@@ -1,4 +1,5 @@
 package Main;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,28 +15,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		List<Conta> contas = new ArrayList<>();
-		ContaComum contac = new ContaComum(20);
+		ContaComum contac = new ContaComum(10);
 		ContaEstudante contae = new ContaEstudante(55);
 		System.out.println("Conta Comum");
 		System.out.println(contac.getSaldo());
 		System.out.println("Conta Estudante");
 		System.out.println(contae.getSaldo());
-		
+
 		contas.add(contae);
 		contas.add(contac);
-		
+
 		for (Conta conta : contas) {
-			if( conta instanceof ContaComum) {
+			if (conta instanceof ContaComum) {
 				((ContaComum) conta).rende();
+				System.out.println("Conata Comum");
+				System.out.println(conta.getSaldo());
+				
+			} else {
+				System.out.println("Conta de Estudante");
+				System.out.println(conta.getSaldo());
 			}
-			System.out.println(conta.getSaldo());
 		}
-		
-		
-	
-		
 
 	}
 
